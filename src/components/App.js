71b4547ce9db4a5b707ad71d59c78b5a2b64ff84
cdrishinski <cdrishinski@gmail.com>
2019-12-18 18:24:01 -0700

    added new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainContainer from './MainContainer';
+import Nav from './Nav';
+
+
 
 function App() {
   return (
-    <div>
-      <image src={logo} ></image>
+    <div className="App">
+      <MainContainer />
+      <Nav />
     </div>
   )
 }
