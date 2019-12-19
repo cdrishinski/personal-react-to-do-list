06cac516b536dbb437f74e390248705f8a09d4d4
cdrishinski <cdrishinski@gmail.com>
2019-12-18 18:21:27 -0700

    about to add new components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <image src={logo} ></image>
+    </div>
+  )
+}
+export default App;
